3361aadfe50b95d54e6a3e0a97c7b30c45a7e6f7
Hiliana Angulo <ahiliana@gmail.com>
2021-06-23 17:41:36 -0400

    Se agrega barra exitGame, se da completa funcionalidad al boardGame y se arregla el código

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import App from './components/App.js';
 
 
-
 document.getElementById('buttonPlay').addEventListener("click", ()=>{
     document.getElementById("blockOne").style.display = "none";
-    
     document.getElementById("container").appendChild(App());
 });
+
 
